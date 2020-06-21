89f8f4095e927080eeab0e2a181485da1c5a71d9
Marader19 <marader191@yandex.ru>
2020-06-21 13:08:43 +0300

    Привели змейку в движение

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -13,6 +14,7 @@ namespace Snake
             Console.SetWindowSize(1, 1);
             Console.SetBufferSize(80, 25);
             Console.SetWindowSize(80, 25);
+            // Отрисовка рамочки
             HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
             VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
@@ -24,12 +26,16 @@ namespace Snake
             rightLine.Draw();
 
 
-
+            // Отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
+            snake.Move();
+            Thread.Sleep(300);
+            
 
             Console.ReadLine();
+            
         }
     }
-}
+} 
--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -10,8 +10,10 @@ namespace Snake
 {
     class Snake : Figure
     {
-        public Snake(Point tail, int lenght, Direction direction)
+        Direction direction;
+        public Snake(Point tail, int lenght, Direction _direction)
         {
+            direction = _direction;
             pList = new List<Point>();
             for (int i = 0; i < lenght; i++)
             {
@@ -19,6 +21,26 @@ namespace Snake
                 p.Move(i, direction);
                 pList.Add(p);
             }
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
         }
     }
 }
