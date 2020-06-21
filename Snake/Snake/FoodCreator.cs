edb76eec864231e541c8107ec43c4c4df80cdb94
Marader19 <marader191@yandex.ru>
2020-06-21 14:07:23 +0300

    Добавили еду и функцию есть

---
+++ Snake/Snake/FoodCreator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class FoodCreator
+    {
+        int mapWidght;
+        int mapHeight;
+        char sym;
+
+        Random random = new Random();
+        public FoodCreator(int mapWidght, int mapHeight, char sym)
+        {
+            this.mapWidght = mapWidght;
+            this.mapHeight = mapHeight;
+            this.sym = sym;
+        }
+
+        public Point CreateFood()
+        {
+            int x = random.Next(2, mapWidght - 2);
+            int y = random.Next(2, mapHeight - 2);
+            return new Point(x, y, sym);
+        }
+    }
+}
--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -48,6 +48,12 @@ namespace Snake
             }
         }
 
+
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
