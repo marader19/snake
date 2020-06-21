070a343a471c01e4cc67f0c9e5b968a03c383664
Marader19 <marader191@yandex.ru>
2020-06-21 15:15:57 +0300

    Завершили курс

--- Snake/Snake/FoodCreator.cs
+++ Snake/Snake/FoodCreator.cs
@@ -8,9 +8,9 @@ namespace Snake
 {
     class FoodCreator
     {
-        int mapWidght;
-        int mapHeight;
-        char sym;
+        private int mapWidght;
+        private int mapHeight;
+        private char sym;
 
         Random random = new Random();
         public FoodCreator(int mapWidght, int mapHeight, char sym)
